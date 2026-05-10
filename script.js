40e8ffae2a47ae8295da31e2fe3a233d10f36c77
Sourabh Singh <67355597+Sourabhsingh021@users.noreply.github.com>
2026-05-10 17:06:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+codes.forEach((input, index) => {
+
+    input.addEventListener("input", () => {
+        // move to next input
+        if(input.value && index < codes.length - 1){
+            codes[index + 1].focus();
+        }
+
+    });
+    // move to previous input
+    input.addEventListener("keydown", (e)=>{
+        if(e.key === "Backspace" && index > 0){
+            codes[index].value = ""
+            codes[index-1].focus();
+        }
+    })
+
+});
